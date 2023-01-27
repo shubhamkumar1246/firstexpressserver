ec4754ebea5bf49417a1f718afe18372ee95187c
VC <sr3867140@gmail.com>
2023-01-27 11:23:26 +0530

    views

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
+
+const path = require('path')
 const app = express()
 const port = 3000
 
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname, 'views'))
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index',{
+    title: "node class"
+  })
 })
-
+//app.usestatic(path.join(__dirname, 'public'))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
